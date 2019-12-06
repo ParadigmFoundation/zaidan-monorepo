0fcd36033493ec16167da5a81d744777dc8b38ca
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-12-06 15:45:39 -0600

    Updating test

--- services/watcher/server/server_test.go
+++ services/watcher/server/server_test.go
@@ -12,7 +12,7 @@ func TestWatchTransaction(t *testing.T) {
 	ws := WatcherServer{ GethAddress: "https://ropsten.infura.io" } //TODO geth
 	ws.Init() //TODO stub?
 	transaction, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf98210"})
-	assert.Equal(t, "0", transaction.TxStatus)
+	assert.Equal(t, false, transaction.IsPending)
 	assert.Equal(t, nil, err)
 }
 
