dfa43c6c4099292fb1232bd255a5de437da075ab
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-12-11 17:13:42 -0600

    capturing and passing down a maker endpoint

--- services/watcher/cmd/main.go
+++ services/watcher/cmd/main.go
@@ -16,6 +16,7 @@ import (
 
 var (
 	gethAddress string
+	makerEndpoint string
 	port int
 
 	cmd = &cobra.Command{
@@ -35,6 +36,7 @@ func configureFlags() {
 	flags := cmd.PersistentFlags()
 	flags.StringVar(&gethAddress, "geth", "wss://eth-ropsten.ws.alchemyapi.io/ws/nUUajaRKoZM-645b32rSRMwNVhW2EP3w", "Geth endpoint")
 	flags.IntVarP(&port, "port", "p", 5001, "gRPC listen port")
+	flags.StringVar(&makerEndpoint, "maker", "localhost:5002", "Maker gRPC endpoint")
 }
 
 func startup (cmd *cobra.Command, args []string) {
@@ -44,7 +46,7 @@ func startup (cmd *cobra.Command, args []string) {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	watcherServer := &server.WatcherServer{ GethAddress: gethAddress }
+	watcherServer := &server.WatcherServer{ GethAddress: gethAddress, MakerEndpoint: makerEndpoint }
 	if err := watcherServer.Init(); err != nil {
 		log.Fatalf("failed to initialize server: %v", err)
 	}
