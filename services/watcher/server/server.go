d9912680c6840278a70941d04b12dc51599d6219
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2019-12-11 17:19:44 -0600

    Updating to take action when transaction is seen.  Adding quote id to be passed along. Various other changes.

--- services/watcher/server/server.go
+++ services/watcher/server/server.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	pb "../proto"
+	"../watching"
 	"context"
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
@@ -11,7 +13,9 @@ import (
 
 type WatcherServer struct {
 	GethAddress string
+	MakerEndpoint string
 	GethClient *ethclient.Client
+	TxWatching *watching.TxWatching
 }
 
 func (s *WatcherServer) WatchTransaction(ctx context.Context, in *pb.WatchTransactionRequest) (*pb.WatchTransactionResponse, error) {
@@ -20,23 +24,50 @@ func (s *WatcherServer) WatchTransaction(ctx context.Context, in *pb.WatchTransa
 
 	_, isPending, err:= s.GethClient.TransactionByHash(context.Background(), txHash)
 	if err != nil {
+		log.Println("Transaction lookup failure", err)
 		return nil, err
 		//TODO do a thing
 	}
 
-	//TODO if pending start listener if not make calls
+	isWatched := false
 
-	return &pb.WatchTransactionResponse{ IsPending: isPending }, nil
+	if isPending {
+		for _, currentHash := range s.TxWatching.WatchedTransactions {
+			if currentHash.TxHash == txHash {
+				isWatched = true
+			}
+		}
+		if !isWatched {
+			s.TxWatching.WatchedTransactions = append(s.TxWatching.WatchedTransactions, watching.WatchedTransaction{TxHash: txHash, QuoteId: in.QuoteId})
+		}
+	} else {
+		log.Println("Transaction mined")
+		// TODO: if pending start listener if not make calls
+	}
+
+	return &pb.WatchTransactionResponse{ IsPending: isPending, TxHash: txHash.String(), QuoteId: in.QuoteId }, nil
 }
 
 func (s *WatcherServer) Init() error {
 	client, err := ethclient.Dial(s.GethAddress)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to connect to rpc" + err.Error())
+	}
+
+	txWatching := watching.TxWatching{
+		EthClient: client,
+		MakerEndpoint: s.MakerEndpoint,
+	}
+
+	err = txWatching.Init()
+
+	if err != nil {
+		return fmt.Errorf("failed to start tx watching" + err.Error())
 	}
 
 	s.GethClient = client
+	s.TxWatching = &txWatching
 
 	return nil
 }
--- services/watcher/watching/watching.go
+++ services/watcher/watching/watching.go
@@ -1,54 +1,95 @@
 package watching
 
 import (
+	pb "../proto"
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/log"
+	"google.golang.org/grpc"
+	"log"
 )
 
-type TxWatching struct {
-	Context   context.Context
-	EthClient *ethclient.Client
-	TxHashes  []common.Hash
+type WatchedTransaction struct {
+	TxHash common.Hash
+	QuoteId string
 }
 
+type TxWatching struct {
+	EthClient           *ethclient.Client
+	WatchedTransactions []WatchedTransaction
+	MakerEndpoint       string
+	MakerClient         pb.MakerClient
+}
+
+var bg = context.Background()
+
 func (txW *TxWatching) Init() error {
+	conn, err := grpc.Dial(txW.MakerEndpoint, grpc.WithInsecure())
+
+	if err != nil {
+		return fmt.Errorf("failed to connect maker client " + err.Error())
+	}
+
+	txW.MakerClient = pb.NewMakerClient(conn)
 
 	headerChan := make(chan *types.Header)
-	sub, err := txW.EthClient.SubscribeNewHead(context.Background(), headerChan)
+	sub, err := txW.EthClient.SubscribeNewHead(bg, headerChan)
 	if err != nil {
-		return fmt.Errorf("failed to subscribe" + err.Error())
-
+		return fmt.Errorf("failed to subscribe " + err.Error())
 	}
-	
-	go txW.watchBlock(headerChan)
-	sub.Err() //TODO use error channel to attempt to reset connect a few times before crashing
+
+	// TODO use error channel to attempt to reset connect a few times before crashing
+	go txW.watchBlock(headerChan, sub.Err())
 
 	return nil
 }
 
-func (txW *TxWatching) watchBlock(headers chan *types.Header) {
+func (txW *TxWatching) watchBlock(headerChannel <-chan *types.Header, errorChannel <-chan error) {
 	for {
 		select {
-			case headers := <-headers:
-				block, err:= txW.EthClient.BlockByNumber(context.Background(), headers.Number)
-				if err != nil {
-					fmt.Println(err)
+			case errors := <- errorChannel:
+				//TODO reset connection
+				log.Fatal("Subscription error!", errors, len(headerChannel))
+			case headers, ok := <- headerChannel:
+				if (!ok) {
+					fmt.Println("Headers died: ", len(headerChannel), ok)
 				}
+
+				block, err := txW.EthClient.BlockByNumber(bg, headers.Number)
+				if err != nil {
+					fmt.Println("Error getting block number: ", headers.Number.String(), err) //TODO Error handling
+					return
+				}
+
 				var foundTxs []common.Hash
+
 				for _, blockTx := range block.Transactions() {
 					txHash := blockTx.Hash()
-					for wI, watchTx := range txW.TxHashes {
-						if txHash == watchTx {
-							log.Info("Found ", txHash, " in Block #", block.Number().String())
+					for wI, watchTx := range txW.WatchedTransactions {
+						if txHash == watchTx.TxHash {
+							log.Println("Found ", txHash.String(), " in Block #", block.Number().String())
 							foundTxs = append(foundTxs, txHash)
-							txW.TxHashes[wI] = txW.TxHashes[len(txW.TxHashes)-1]
-							txW.TxHashes = txW.TxHashes[:len(txW.TxHashes)-1]
+							txW.WatchedTransactions[wI] = txW.WatchedTransactions[len(txW.WatchedTransactions)-1]
+							txW.WatchedTransactions = txW.WatchedTransactions[:len(txW.WatchedTransactions)-1]
 
-							//TODO CALL TO CONFIRM
+							receipt, err := txW.EthClient.TransactionReceipt(bg, txHash)
+							if err != nil {
+								fmt.Println(err) //TODO Error handling
+							}
+
+							//TODO CALL TO CONFIRM  //TODO Error handling
+							_, err = txW.MakerClient.OrderStatusUpdate(context.Background(), &pb.OrderStatusUpdateRequest{
+								QuoteId: watchTx.QuoteId,
+								Status:  uint32(receipt.Status),
+							})
+
+							if err != nil {
+								log.Println("Failure calling maker: ", err)
+							}
+
+							break
 						}
 					}
 				}
