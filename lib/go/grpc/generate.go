4f0070a809c6a5c231c90747388c06fcb0d83782
Henry Harder <henry@paradigm.market>
2019-12-12 13:54:22 -0800

    lib/go: separate types and services

--- lib/go/grpc/generate.go
+++ lib/go/grpc/generate.go
@@ -1,3 +1,4 @@
 package grpc
 
-//go:generate protoc --go_out=plugins=grpc:. --proto_path=../../../proto types.proto
+//go:generate protoc --go_out=. --proto_path=../../../proto types.proto
+//go:generate protoc --go_out=plugins=grpc:. --proto_path=../../../proto services.proto
