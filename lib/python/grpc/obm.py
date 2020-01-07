d4ab53ee78542119705b8e43465c36c59235024c
Henry Harder <henry@paradigm.market>
2020-01-06 22:56:48 -0800

    lib/python: add make commands and improve example

--- lib/python/grpc/obm.py
+++ lib/python/grpc/obm.py
@@ -1,14 +1,19 @@
 import grpc
-from types_pb2_grpc import OrderBookManagerStub
+import json
+from services_pb2_grpc import OrderBookManagerStub
 from types_pb2 import OrderBookRequest
 
 # Connect to the OBM server
-channel = grpc.insecure_channel('localhost:8000')
+channel = grpc.insecure_channel('localhost:8001')
 stub = OrderBookManagerStub(channel)
 
 # Build the request
-req = OrderBookRequest(exchange = "binance", symbol = "BTC/USDT")
+req = OrderBookRequest(exchange = "coinbase", symbol = "ETH/USD")
 
 # Call the server
 response = stub.OrderBook(req)
-print(response)
+
+bid_book = [[x.price, x.quantity] for x in response.bids]
+ask_book = [[x.price, x.quantity] for x in response.asks]
+
+print(json.dumps({"asks":ask_book, "bids":bid_book}))
