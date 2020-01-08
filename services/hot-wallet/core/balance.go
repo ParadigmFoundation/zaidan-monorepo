47834df26bdae5dfc6c1a4f2ef401efa2994ebe8
Henry Harder <henry@paradigm.market>
2020-01-08 15:12:54 -0500

    hot-wallet: separate ether and token balance methods

--- services/hot-wallet/core/balance.go
+++ services/hot-wallet/core/balance.go
@@ -2,41 +2,31 @@ package core
 
 import (
 	"context"
-	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
 
 	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/grpc"
-	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/zrx"
 )
 
-// GetTokenBalance implements grpc.HotWalletServer
-func (hw *HotWallet) GetBalance(ctx context.Context, req *grpc.GetBalanceRequest) (*grpc.GetBalanceResponse, error) {
+// GetEtherBalance implements grpc.HotWalletServer
+func (hw *HotWallet) GetEtherBalance(ctx context.Context, req *grpc.GetBalanceRequest) (*grpc.GetBalanceResponse, error) {
 	owner := common.HexToAddress(req.OwnerAddress)
-	token := common.HexToAddress(req.TokenAddress)
-
-	// special case for checking Ether balance
-	if token == zrx.NULL_ADDRESS {
-		return hw.getEtherBalance(ctx, owner)
-	} else {
-		return hw.getTokenBalance(ctx, owner, token)
-	}
-}
-
-func (hw *HotWallet) getEtherBalance(ctx context.Context, owner common.Address) (*grpc.GetBalanceResponse, error) {
 	balance, err := hw.provider.Client().BalanceAt(ctx, owner, nil)
 	if err != nil {
 		return nil, err
 	}
 
 	return &grpc.GetBalanceResponse{
-		OwnerAddress: strings.ToLower(owner.Hex()),
-		TokenAddress: strings.ToLower((common.Address{}).Hex()),
+		OwnerAddress: grpc.NormalizeAddress(owner),
 		Balance:      balance.String(),
 	}, nil
 }
 
-func (hw *HotWallet) getTokenBalance(ctx context.Context, owner common.Address, token common.Address) (*grpc.GetBalanceResponse, error) {
+// GetTokenBalance implements grpc.HotWalletServer
+func (hw *HotWallet) GetTokenBalance(ctx context.Context, req *grpc.GetBalanceRequest) (*grpc.GetBalanceResponse, error) {
+	owner := common.HexToAddress(req.OwnerAddress)
+	token := common.HexToAddress(req.TokenAddress)
+
 	devUtils := hw.zrxHelper.DevUtils()
 	assetData, err := devUtils.EncodeERC20AssetData(nil, token)
 	if err != nil {
@@ -49,8 +39,8 @@ func (hw *HotWallet) getTokenBalance(ctx context.Context, owner common.Address, 
 	}
 
 	return &grpc.GetBalanceResponse{
-		OwnerAddress: strings.ToLower(owner.Hex()),
-		TokenAddress: strings.ToLower(token.Hex()),
+		OwnerAddress: grpc.NormalizeAddress(owner),
+		TokenAddress: grpc.NormalizeAddress(token),
 		Balance:      balance.String(),
 	}, nil
 }
