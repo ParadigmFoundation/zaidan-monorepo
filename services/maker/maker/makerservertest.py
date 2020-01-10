5360f71adb0f2c9cfb1cfe72b3fa5011a90081d8
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-09 20:00:11 -0500

    grpc files

---
+++ services/maker/maker/makerservertest.py
@@ -0,0 +1,20 @@
+import grpc
+from types_pb2_grpc import MakerStub
+from types_pb2 import GetQuoteRequest, CheckQuoteRequest
+
+# Connect to the OBM server
+channel = grpc.insecure_channel('localhost:50051')
+stub = MakerStub(channel)
+
+# Build the request
+req = GetQuoteRequest(maker_asset='ETH', taker_asset='DAI', maker_size='1')
+
+# Call the server
+response = stub.GetQuote(req)
+print(response)
+
+req = CheckQuoteRequest(quote_id=response.quote_id)
+
+response = stub.CheckQuote(req)
+print(response)
+
