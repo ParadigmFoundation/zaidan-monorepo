beb02b762e1b503a542a7b61a3e4ca723ce5e7e7
Henry Harder <henry@paradigm.market>
2020-01-11 22:08:42 -0500

    hot-wallet: clean up transaction impl.

--- services/hot-wallet/core/transaction.go
+++ services/hot-wallet/core/transaction.go
@@ -23,23 +23,14 @@ func (hw *HotWallet) SendTransaction(ctx context.Context, req *grpc.SendTransact
 		return nil, err
 	}
 
-	gasPrice := new(big.Int)
-	if req.GasPrice == "" {
-		gp, err := hw.provider.Client().SuggestGasPrice(ctx)
-		if err != nil {
-			return nil, err
-		}
-		gasPrice = gp
+	gasPrice, err := hw.parseGasPrice(ctx, req.GasPrice)
+	if err != nil {
+		return nil, err
 	}
 
-	value := new(big.Int)
-	if req.Value == "" {
-		value = big.NewInt(0)
-	} else {
-		_, ok := value.SetString(req.Value, 10)
-		if !ok {
-			return nil, status.Error(codes.InvalidArgument, "unable to parse 'value'")
-		}
+	value, err := hw.parseValue(req.Value)
+	if err != nil {
+		return nil, err
 	}
 
 	nonce, err := hw.provider.Nonce(ctx, hw.makerAddress)
@@ -47,21 +38,9 @@ func (hw *HotWallet) SendTransaction(ctx context.Context, req *grpc.SendTransact
 		return nil, err
 	}
 
-	msg := ethereum.CallMsg{
-		From:     hw.makerAddress,
-		To:       &to,
-		Gas:      req.GasLimit,
-		GasPrice: gasPrice,
-		Value:    value,
-		Data:     req.Data,
-	}
-
-	gasLimit := msg.Gas
-	if gasLimit == 0 {
-		gasLimit, err = hw.provider.Client().EstimateGas(ctx, msg)
-		if err != nil {
-			return nil, err
-		}
+	gasLimit, err := hw.getGasLimit(ctx, to, req.GasLimit, value, gasPrice, req.Data)
+	if err != nil {
+		return nil, err
 	}
 
 	tx := types.NewTransaction(nonce, to, value, gasLimit, gasPrice, req.Data)
@@ -78,4 +57,56 @@ func (hw *HotWallet) SendTransaction(ctx context.Context, req *grpc.SendTransact
 		TransactionHash: stx.Hash().Hex(),
 		SentAt:          time.Now().Unix(),
 	}, nil
+}
+
+// parses gas price from request string, if empty string, uses suggested gas price
+func (hw *HotWallet) parseGasPrice(ctx context.Context, raw string) (*big.Int, error) {
+	if raw == "" {
+		gasPrice, err := hw.provider.Client().SuggestGasPrice(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return gasPrice, nil
+	} else {
+		gasPrice, ok := new(big.Int).SetString(raw, 10)
+		if !ok {
+			return nil, status.Error(codes.InvalidArgument, "unable to parse 'gasPrice'")
+		}
+		return gasPrice, nil
+	}
+}
+
+// parses ether value from request string (if raw is empty string, returns 0)
+func (hw *HotWallet) parseValue(raw string) (*big.Int, error) {
+	if raw == "" {
+		return big.NewInt(0), nil
+	} else {
+		value, ok := new(big.Int).SetString(raw, 10)
+		if !ok {
+			return nil, status.Error(codes.InvalidArgument, "unable to parse 'value'")
+		}
+		return value, nil
+	}
+}
+
+// estimates gas required for transaction if gas is 0, otherwise returns gas
+func (hw *HotWallet) getGasLimit(ctx context.Context, to common.Address, gas uint64, val *big.Int, gasPrice *big.Int, data []byte) (uint64, error) {
+	msg := ethereum.CallMsg{
+		From:     hw.makerAddress,
+		To:       &to,
+		Gas:      gas,
+		GasPrice: gasPrice,
+		Value:    val,
+		Data:     data,
+	}
+
+	var err error
+	gasLimit := msg.Gas
+	if gasLimit == 0 {
+		gasLimit, err = hw.provider.Client().EstimateGas(ctx, msg)
+		if err != nil {
+			return 0, err
+		}
+	}
+	return gasLimit, nil
 }
