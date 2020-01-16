eab4519593e6f2b206e1f6a9b4b03c8a1eda219e
L-Kov <liam@paradigm.market>
2020-01-15 19:10:55 -0700

    fixing tests gzip not deterministic

--- services/maker/maker/test_common_utils.py
+++ services/maker/maker/test_common_utils.py
@@ -7,16 +7,16 @@ class CommonUtilsTest(unittest.TestCase):
 
     def test_is_valid_uuid(self) -> None:
         uuid_str = str(uuid4())
-        self.assetEqual(is_valid_uuid(uuid_str), True)
+        self.assertEqual(is_valid_uuid(uuid_str), True)
     
     def test_encode_to_bytes(self) -> None:
         test_obj = {'key_1': 0, 'key_2': 2}
-        test_bytes = b'H4sIAP6+H14C/6tWyk6tjDdUslIw0FEAs42AbKNaADJGwwcYAAAA'
-        self.assertEqual(encode_to_bytes(test_obj), test_bytes)
+        test_bytes_det_str = '6tWyk6tjDdUslIw0FEAs42AbKNaADJGwwcYAAAA'
+        self.assertEqual(encode_to_bytes(test_obj).decode("utf-8").split('/')[1], test_bytes_det_str)
     
     def test_decode_from_bytes(self) -> None:
         test_obj = {'key_1': 0, 'key_2': 2}
-        test_bytes = b'H4sIAP6+H14C/6tWyk6tjDdUslIw0FEAs42AbKNaADJGwwcYAAAA'
+        test_bytes = b'H4sIAArDH14C/6tWyk6tjDdUslIw0FEAs42AbKNaADJGwwcYAAAA'
         self.assertEqual(decode_from_bytes(test_bytes), test_obj)
     
 if __name__ == '__main__':
