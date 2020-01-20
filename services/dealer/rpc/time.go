085c9b719ce603b3e2e596c16af094d667a92f45
Henry Harder <henry@paradigm.market>
2020-01-20 17:48:36 -0500

    dealer/rpc: make response type private

--- services/dealer/rpc/time.go
+++ services/dealer/rpc/time.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-// TimeResponse represents the response from the dealer_time endpoint
-type TimeResponse struct {
+// timeResponse represents the response from the dealer_time endpoint
+type timeResponse struct {
 	dealerTime int64
 
 	// pointer so the default is null instead of 0
@@ -14,12 +14,12 @@ type TimeResponse struct {
 }
 
 // MarshalJSON implements json.Marshaller. TimeResponse is marshalled as an array
-func (tr *TimeResponse) MarshalJSON() ([]byte, error) {
+func (tr *timeResponse) MarshalJSON() ([]byte, error) {
 	return json.Marshal([]interface{}{tr.dealerTime, tr.diff})
 }
 
 // Time implements the dealer_time method
-func (svc *Service) Time(clientTime *int64) *TimeResponse {
+func (svc *Service) Time(clientTime *int64) *timeResponse {
 	ts := time.Now().UnixNano() / 1e6 // conversion to ms
 	var diff *int64
 
@@ -29,5 +29,5 @@ func (svc *Service) Time(clientTime *int64) *TimeResponse {
 		diff = &tmp
 	}
 
-	return &TimeResponse{dealerTime: ts, diff: diff}
+	return &timeResponse{dealerTime: ts, diff: diff}
 }
