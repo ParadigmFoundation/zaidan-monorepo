ab76dab26950d0098a08f2932222e5414026e24d
Henry Harder <henry@paradigm.market>
2020-01-22 15:51:55 -0500

    lib/go/grpc: add transaction helpers

--- lib/go/grpc/transaction.go
+++ lib/go/grpc/transaction.go
@@ -3,8 +3,10 @@ package grpc
 import (
 	fmt "fmt"
 	"math/big"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/zrx"
 )
@@ -32,4 +34,39 @@ func (etx ExecuteZeroExTransactionRequest) ToZeroExTransaction() (*zrx.Transacti
 		SignerAddress:         common.HexToAddress(etx.SignerAddress),
 		Data:                  etx.Data,
 	}, nil
+}
+
+func (ztx *ZeroExTransaction) ToZeroExTransaction() (*zrx.Transaction, error) {
+	salt, ok := new(big.Int).SetString(ztx.Salt, 10)
+	if !ok {
+		return nil, fmt.Errorf("unable to parse 'salt' (%v)", ztx.Salt)
+	}
+
+	gasPrice, ok := new(big.Int).SetString(ztx.GasPrice, 10)
+	if !ok {
+		return nil, fmt.Errorf("unable to parse 'gas_price' (%v)", ztx.GasPrice)
+	}
+
+	callData, err := hexutil.Decode(ztx.Data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &zrx.Transaction{
+		Salt:                  salt,
+		ExpirationTimeSeconds: big.NewInt(ztx.ExpirationTimeSeconds),
+		GasPrice:              gasPrice,
+		SignerAddress:         common.HexToAddress(ztx.SignerAddress),
+		Data:                  callData,
+	}, nil
+}
+
+func ZeroExTransactionToProto(ztx *zrx.Transaction) *ZeroExTransaction {
+	return &ZeroExTransaction{
+		Salt:                  ztx.Salt.String(),
+		ExpirationTimeSeconds: ztx.ExpirationTimeSeconds.Int64(),
+		GasPrice:              ztx.GasPrice.String(),
+		SignerAddress:         strings.ToLower(ztx.SignerAddress.Hex()),
+		Data:                  hexutil.Encode(ztx.Data),
+	}
 }
