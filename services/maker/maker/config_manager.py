4c2203f1b386494b9602fe9e6091de4496b7211c
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-23 12:59:32 -0500

    changing names

--- services/maker/maker/config_manager.py
+++ services/maker/maker/config_manager.py
@@ -1,20 +1,25 @@
 import os
 import json
 
-ASSET_CONFIG_FILE = os.environ.get('ASSET_CONFIG_FILE', 'maker/asset_config.json')
+CONFIG_FILE = os.environ.get('CONFIG_FILE', 'maker/config.json')
 
-class AssetData():
+class ConfigManager():
 
     address_to_ticker = {}
     ticker_to_pricing_data = {}
 
     def __init__(self) -> None:
-        with open(ASSET_CONFIG_FILE) as f:
+        with open(CONFIG_FILE) as f:
             asset_config_f = json.load(f)
             for asset_data in asset_config_f['assets']:
                 self.address_to_ticker[asset_data['address']] = asset_data['symbol']
                 self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
                 self.ticker_to_pricing_data[asset_data['symbol']]['decimals'] = asset_data['decimals']
+            self.premium = float(asset_config_f['premium'])
+            self.validity_length = int(asset_config_f['validity_length'])
+            self.exchange_fees = asset_config_f['exchange_fees']
+            for exchange in self.exchange_fees.keys():
+                self.exchange_fees[exchange] = float(self.exchange_fees[exchange])
 
     def get_ticker_with_address(self, address: str) -> str:
         return self.address_to_ticker[address]
--- services/maker/tests/test_config_manager.py
+++ services/maker/tests/test_config_manager.py
@@ -3,20 +3,20 @@ import sys
 
 try:
     sys.path.append('../maker')
-    from asset_data import AssetData
+    from config_manager import AssetData
 except:
     try:
         sys.path.append('../maker/maker')
-        from asset_data import AssetData
+        from config_manager import AssetData
     except Exception as error:
         raise Exception('failed to import asset data: {}'.format(error.args))
 
 class TestAssetData(unittest.TestCase):
 
-    def test_asset_data_get_ticker_with_address(self) -> None:
+    def test_config_manager_get_ticker_with_address(self) -> None:
         WETH_ADDRESS = '0x0b1ba0af832d7c05fd64161e0db78e85978e8082'
-        asset_data = AssetData()
-        self.assertEqual(asset_data.get_ticker_with_address(WETH_ADDRESS), 'WETH')
+        config_manager = AssetData()
+        self.assertEqual(config_manager.get_ticker_with_address(WETH_ADDRESS), 'WETH')
 
 if __name__ == '__main__':
     unittest.main()
