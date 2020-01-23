d965281e362cb9a4384e5b012f95830112357ca2
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-22 23:21:20 -0500

    adding desimals to asset data object

--- services/maker/maker/asset_data.py
+++ services/maker/maker/asset_data.py
@@ -18,12 +18,14 @@ class AssetData():
                 for asset_data in asset_config_f['assets']:
                     self.address_to_ticker[asset_data['address']] = asset_data['symbol']
                     self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
+                    self.ticker_to_pricing_data[asset_data['symbol']]['decimals'] = asset_data['decimals']
         except:
             with open(ASSET_CONFIG_FILE) as f:
                 asset_config_f = json.load(f)
                 for asset_data in asset_config_f['assets']:
                     self.address_to_ticker[asset_data['address']] = asset_data['symbol']
                     self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
+                    self.ticker_to_pricing_data[asset_data['symbol']]['decimals'] = asset_data['decimals']
 
     def get_ticker_with_address(self, address: str) -> str:
         return self.address_to_ticker[address]
