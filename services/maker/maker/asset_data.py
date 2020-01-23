f797c96baa161f271dd2d787c366ae45c5a16a8b
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-22 23:13:43 -0500

    changes to config file and class

--- services/maker/maker/asset_data.py
+++ services/maker/maker/asset_data.py
@@ -6,12 +6,15 @@ ASSET_CONFIG_FILE = os.environ.get('ASSET_CONFIG_FILE', 'asset_config.json')
 class AssetData():
 
     address_to_ticker = {}
+    ticker_to_pricing_data = {}
 
     def __init__(self) -> None:
         with open(ASSET_CONFIG_FILE) as f:
             asset_config_f = json.load(f)
             for asset_data in asset_config_f['assets']:
                 self.address_to_ticker[asset_data['address']] = asset_data['symbol']
+                self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
+
 
     def get_ticker_with_address(self, address: str) -> str:
         return self.address_to_ticker[address]
