f865d3a0a4b41b566b48a9690e55d88d34d356c5
Henry Harder <henry@paradigm.market>
2020-01-23 11:04:36 -0500

    lib/go/grpc: regenerate types, update methods

--- lib/go/grpc/transaction.go
+++ lib/go/grpc/transaction.go
@@ -11,31 +11,6 @@ import (
 	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/zrx"
 )
 
-func (etx ExecuteZeroExTransactionRequest) ToZeroExTransaction() (*zrx.Transaction, error) {
-	salt, ok := new(big.Int).SetString(etx.Salt, 10)
-	if !ok {
-		return nil, fmt.Errorf("unable to parse 'salt' (%v)", etx.Salt)
-	}
-
-	expiration, ok := new(big.Int).SetString(etx.ExpirationTimeSeconds, 10)
-	if !ok {
-		return nil, fmt.Errorf("unable to parse 'expirationTimeSeconds' (%v)", etx.ExpirationTimeSeconds)
-	}
-
-	gasPrice, ok := new(big.Int).SetString(etx.GasPrice, 10)
-	if !ok {
-		return nil, fmt.Errorf("unable to parse 'gasPrice' (%v)", etx.GasPrice)
-	}
-
-	return &zrx.Transaction{
-		Salt:                  salt,
-		ExpirationTimeSeconds: expiration,
-		GasPrice:              gasPrice,
-		SignerAddress:         common.HexToAddress(etx.SignerAddress),
-		Data:                  etx.Data,
-	}, nil
-}
-
 func (ztx *ZeroExTransaction) ToZeroExTransaction() (*zrx.Transaction, error) {
 	salt, ok := new(big.Int).SetString(ztx.Salt, 10)
 	if !ok {
