fa41dd698992133aeec413ad4f28f5347f4251ab
Henry Harder <henry@paradigm.market>
2020-01-23 12:20:31 -0500

    maker: remove comments/try-catch block

--- services/maker/maker/asset_data.py
+++ services/maker/maker/asset_data.py
@@ -2,9 +2,6 @@ import os
 import json
 
 ASSET_CONFIG_FILE = os.environ.get('ASSET_CONFIG_FILE', 'maker/asset_config.json')
-path_to_current_file = os.path.realpath(__file__)
-current_directory = os.path.split(path_to_current_file)[0]
-path_to_file = os.path.join(current_directory, ASSET_CONFIG_FILE)
 
 class AssetData():
 
@@ -12,20 +9,12 @@ class AssetData():
     ticker_to_pricing_data = {}
 
     def __init__(self) -> None:
-        # try:
         with open(ASSET_CONFIG_FILE) as f:
             asset_config_f = json.load(f)
             for asset_data in asset_config_f['assets']:
                 self.address_to_ticker[asset_data['address']] = asset_data['symbol']
                 self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
                 self.ticker_to_pricing_data[asset_data['symbol']]['decimals'] = asset_data['decimals']
-        # except:
-        #     with open(ASSET_CONFIG_FILE) as f:
-        #         asset_config_f = json.load(f)
-        #         for asset_data in asset_config_f['assets']:
-        #             self.address_to_ticker[asset_data['address']] = asset_data['symbol']
-        #             self.ticker_to_pricing_data[asset_data['symbol']] = asset_data['pricing_data']
-        #             self.ticker_to_pricing_data[asset_data['symbol']]['decimals'] = asset_data['decimals']
 
     def get_ticker_with_address(self, address: str) -> str:
         return self.address_to_ticker[address]
