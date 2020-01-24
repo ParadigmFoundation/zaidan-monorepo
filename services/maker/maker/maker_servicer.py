0cd66c566bb337e2ad41ed1cb118cc75ab1d0565
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-24 13:19:13 -0500

    initial commit

--- services/maker/maker/maker_servicer.py
+++ services/maker/maker/maker_servicer.py
@@ -33,9 +33,10 @@ class MakerServicer(services_pb2_grpc.MakerServicer):
         if False not in self.risk_utils.risk_checks(taker_asset, maker_asset, trading_taker_size, quote_info, self.test):
             quote_id = str(uuid.uuid4())
 
-            self.redis_interface.add_quote(quote_id, {'expiration':expiry_timestamp,
-                                       'taker_asset':request.taker_asset, 'maker_asset':request.maker_asset,
-                                       'taker_size':quote_info['taker_size'], 'maker_size':quote_info['maker_size']})
+            if not request.price_only:
+                self.redis_interface.add_quote(quote_id, {'expiration':expiry_timestamp,
+                                           'taker_asset':request.taker_asset, 'maker_asset':request.maker_asset,
+                                           'taker_size':quote_info['taker_size'], 'maker_size':quote_info['maker_size']})
 
             quote_info = PricingUtils.format_quote(taker_asset, maker_asset, quote_info)
 
