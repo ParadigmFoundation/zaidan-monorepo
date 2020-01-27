4b8f63d43dbb7b52e7d9608d94d10cade8425607
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2020-01-27 16:06:28 -0600

    Fixing naming issues and using other log with watcher test binaries.

--- services/watcher/test/test_client.go
+++ services/watcher/test/test_client.go
@@ -10,9 +10,11 @@ import (
 	"strings"
 
 	pb "github.com/ParadigmFoundation/zaidan-monorepo/lib/go/grpc"
-	"github.com/sirupsen/logrus"
+	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/logger"
 	"google.golang.org/grpc"
 )
+
+var log = logger.New("app")
 
 func main() {
 
@@ -20,7 +22,7 @@ func main() {
 	const address = "localhost:5001"
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
-		logrus.Fatal(fmt.Errorf("did not connect: %v", err))
+		log.Fatal(fmt.Errorf("did not connect: %v", err))
 	}
 	defer conn.Close()
 	c := pb.NewWatcherClient(conn)
@@ -28,15 +30,22 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		logrus.Info("Enter txHash:")
+		log.Info("Enter txHash:")
 		txHash, _ := reader.ReadString('\n')
 
-		resp, err := c.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{QuoteId: "Random from test_client", TxHash: strings.Replace(txHash, "\n", "", -1)})
+		resp, err := c.WatchTransaction(
+			context.Background(),
+			&pb.WatchTransactionRequest{
+				QuoteId: "Random from test_client",
+				TxHash: strings.Replace(txHash, "\n", "", -1),
+				StatusUrls: []string{"http://localhost:5002"},
+			},
+		)
 
 		if err != nil {
-			logrus.Error(fmt.Errorf("Error: %s", err))
+			log.Error(fmt.Errorf("Error: %s", err))
 		} else {
-			logrus.Info("Call succeeded: { txHash: ", resp.TxHash, ", quoteId: ", resp.QuoteId, ", isPending: ", fmt.Sprint(resp.IsPending), ", txStatus: ", resp.TxStatus, " }")
+			log.Info("Call succeeded: { txHash: ", resp.TxHash, ", quoteId: ", resp.QuoteId, ", isPending: ", fmt.Sprint(resp.IsPending), ", txStatus: ", resp.TxStatus, " }")
 		}
 	}
 
---
+++ services/watcher/test/test_transaction_status_service.go
@@ -0,0 +1,38 @@
+// +build maker
+
+package main
+
+import (
+	"context"
+	"net"
+	"strconv"
+
+	pb "github.com/ParadigmFoundation/zaidan-monorepo/lib/go/grpc"
+	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/logger"
+	"google.golang.org/grpc"
+)
+
+var log = logger.New("app")
+
+type TransactionStatusServer struct {
+}
+
+func (oss *TransactionStatusServer) TransactionStatusUpdate(c context.Context, r *pb.TransactionStatusUpdateRequest) (*pb.TransactionStatusUpdateResponse, error) {
+	log.Info("Received update for ", r.QuoteId, " Status: ", r.Status)
+	return &pb.TransactionStatusUpdateResponse{ Status: 1 }, nil
+}
+
+func main() {
+	log.Info("Starting Test Maker Endpoint on port 5002")
+	lis, err := net.Listen("tcp", ":"+strconv.Itoa(5002))
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	transactionStatusServer := TransactionStatusServer{}
+
+	pb.RegisterTransactionStatusServer(s, &transactionStatusServer)
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("failed to serve: %v", err)
+	}
+}
