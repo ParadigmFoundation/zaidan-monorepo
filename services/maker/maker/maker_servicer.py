75287e6d8df0ad73f0465d8d057a9df9380e3722
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-28 11:55:15 -0500

    -

--- services/maker/maker/maker_servicer.py
+++ services/maker/maker/maker_servicer.py
@@ -63,9 +63,6 @@ class MakerServicer(services_pb2_grpc.MakerServicer):
 
         return types_pb2.CheckQuoteResponse(quote_id=request.quote_id, is_valid=True, status=200)
 
-    def OrderStatusUpdate(self, request:object, context) -> object:
-        self.risk_utils.order_status_update(request.quote_id, request.status)
-        return types_pb2.OrderStatusUpdateResponse(status=200)
 
     def GetMarkets(self, request:object, context) -> object:
         our_markets = self.config_manager.get_markets(request.maker_asset_address, request.taker_asset_address)
