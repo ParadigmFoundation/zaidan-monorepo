e6432fe8d345059ee5904e9bc001b0235afbfda9
Henry Harder <henry@paradigm.market>
2020-01-29 13:58:20 -0500

    dealer: add custom marshalling for markets response

--- services/dealer/rpc/get_markets.go
+++ services/dealer/rpc/get_markets.go
@@ -16,9 +16,43 @@ type getMarketsResponse struct {
 	perPage int
 }
 
+type jsonMarket struct {
+	MakerAssetAddress   string        `json:"makerAssetAddress"`
+	TakerAssetAddresses []string      `json:"takerAssetAddresses"`
+	TradeInfo           jsonTradeInfo `json:"tradeInfo"`
+	QuoteInfo           jsonQuoteInfo `json:"quoteInfo"`
+}
+
+type jsonTradeInfo struct {
+	ChainID  uint32 `json:"chainId"`
+	GasPrice string `json:"gasPrice"`
+	GasLimit string `json:"gasLimit"`
+}
+
+type jsonQuoteInfo struct {
+	MinSize string `json:"minSize"`
+	MaxSize string `json:"maxSize"`
+}
+
 func (r *getMarketsResponse) MarshalJSON() ([]byte, error) {
+	jsonMarkets := make([]jsonMarket, r.total)
+	for i, mkt := range r.record {
+		jsonMarkets[i] = jsonMarket{
+			MakerAssetAddress:   mkt.MakerAssetAddress,
+			TakerAssetAddresses: mkt.TakerAssetAddresses,
+			TradeInfo: jsonTradeInfo{
+				ChainID:  mkt.TradeInfo.ChainId,
+				GasPrice: mkt.TradeInfo.GasPrice,
+				GasLimit: mkt.TradeInfo.GasLimit,
+			},
+			QuoteInfo: jsonQuoteInfo{
+				MinSize: mkt.QuoteInfo.MinSize,
+				MaxSize: mkt.QuoteInfo.MaxSize,
+			},
+		}
+	}
 	return json.Marshal([]interface{}{
-		r.record, r.total, r.page, r.perPage,
+		jsonMarkets, r.total, r.page, r.perPage,
 	})
 }
 
