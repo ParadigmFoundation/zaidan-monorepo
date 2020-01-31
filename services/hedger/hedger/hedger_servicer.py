c473e006fd93d1eddd9f080fa27652ce992749e5
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-31 12:21:59 -0500

    logger

--- services/hedger/hedger/hedger_servicer.py
+++ services/hedger/hedger/hedger_servicer.py
@@ -8,13 +8,13 @@ class HedgerServicer(services_pb2_grpc.HedgerServicer):
     def __init__(self) -> None:
         services_pb2_grpc.HedgerServicer.__init__(self)
         self.hedger = Hedger()
-        self.logger = logging.Logger()
+        self.logger = logging.Logger('hedger-logger')
 
     
     def HedgeOrder(self, request: object, context) -> object:
 
 
-        self.logger('received request to hedge ordger: ' + str(request.id))
+        self.logger.info('received request to hedge ordger: ' + str(request.id))
 
         order_id = request.id
 
