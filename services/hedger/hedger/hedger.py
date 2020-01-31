6c9e315a75eaefda4ab8678d83eb91b8b8517737
mtmuehl <michael.tim.muehl@gmail.com>
2020-01-31 17:27:34 -0500

    adding in deleted code

--- services/hedger/hedger/hedger.py
+++ services/hedger/hedger/hedger.py
@@ -151,6 +151,8 @@ class Hedger():
         size = round(trade['size'], PRECISION)
         price = trade['price']
         self.logger.info("Received trade", trade)
+        unhedged_positions_update = 0
+        position = self.get_current_position(pair)
 
         # checking if the order is large enough to constitute an acceptable exchange order
         self.logger.info("conditional info")
