59332e38846bf3bdeb22ecd8dfbac313d5065392
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2020-02-05 09:45:31 -0600

    Cleaning up the duplicated code

--- lib/go/logger/module_formatter.go
+++ lib/go/logger/module_formatter.go
@@ -3,16 +3,12 @@ package logger
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"os"
-	"runtime"
 	"sort"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/sirupsen/logrus"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 const (
@@ -52,10 +48,7 @@ func (f FieldMap) resolve(key fieldKey) string {
 
 type ModuleFormatter struct {
 	logrus.TextFormatter
-	BugsnagHook
 	module string
-	isTerminal bool
-	terminalInitOnce sync.Once
 	FieldMap
 }
 
@@ -112,8 +105,6 @@ func (f *ModuleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		b = &bytes.Buffer{}
 	}
 
-	f.terminalInitOnce.Do(func() { f.init(entry) })
-
 	timestampFormat := f.TimestampFormat
 	if timestampFormat == "" {
 		timestampFormat = defaultTimestampFormat
@@ -147,148 +138,6 @@ func (f *ModuleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	return b.Bytes(), nil
 }
 
-//LOGRUS BELOW
-
-//var baseTimestamp time.Time
-//
-//func init() {
-//	baseTimestamp = time.Now()
-//}
-//
-//// TextFormatter formats logs into text
-//type TextFormatter struct {
-//	// Set to true to bypass checking for a TTY before outputting colors.
-//	ForceColors bool
-//
-//	// Force disabling colors.
-//	DisableColors bool
-//
-//	// Override coloring based on CLICOLOR and CLICOLOR_FORCE. - https://bixense.com/clicolors/
-//	EnvironmentOverrideColors bool
-//
-//	// Disable timestamp logging. useful when output is redirected to logging
-//	// system that already adds timestamps.
-//	DisableTimestamp bool
-//
-//	// Enable logging the full timestamp when a TTY is attached instead of just
-//	// the time passed since beginning of execution.
-//	FullTimestamp bool
-//
-//	// TimestampFormat to use for display when a full timestamp is printed
-//	TimestampFormat string
-//
-//	// The fields are sorted by default for a consistent output. For applications
-//	// that log extremely frequently and don't use the JSON formatter this may not
-//	// be desired.
-//	DisableSorting bool
-//
-//	// The keys sorting function, when uninitialized it uses sort.Strings.
-//	SortingFunc func([]string)
-//
-//	// Disables the truncation of the level text to 4 characters.
-//	DisableLevelTruncation bool
-//
-//	// QuoteEmptyFields will wrap empty fields in quotes if true
-//	QuoteEmptyFields bool
-//
-//	// Whether the logger's out is to a terminal
-//	isTerminal bool
-//
-//	// FieldMap allows users to customize the names of keys for default fields.
-//	// As an example:
-//	// formatter := &TextFormatter{
-//	//     FieldMap: FieldMap{
-//	//         FieldKeyTime:  "@timestamp",
-//	//         FieldKeyLevel: "@level",
-//	//         FieldKeyMsg:   "@message"}}
-//	FieldMap FieldMap
-//
-//	// CallerPrettyfier can be set by the user to modify the content
-//	// of the function and file keys in the json data when ReportCaller is
-//	// activated. If any of the returned value is the empty string the
-//	// corresponding key will be removed from json fields.
-//	CallerPrettyfier func(*runtime.Frame) (function string, file string)
-//
-//	terminalInitOnce sync.Once
-//}
-//
-//func (f *ModuleFormatter) init(entry *Entry) {
-//	if entry.Logger != nil {
-//		f.isTerminal = checkIfTerminal(entry.Logger.Out)
-//
-//		if f.isTerminal {
-//			initTerminal(entry.Logger.Out)
-//		}
-//	}
-//}
-//
-//func (f *ModuleFormatter) isColored() bool {
-//	isColored := f.ForceColors || (f.isTerminal && (runtime.GOOS != "windows"))
-//
-//	if f.EnvironmentOverrideColors {
-//		if force, ok := os.LookupEnv("CLICOLOR_FORCE"); ok && force != "0" {
-//			isColored = true
-//		} else if ok && force == "0" {
-//			isColored = false
-//		} else if os.Getenv("CLICOLOR") == "0" {
-//			isColored = false
-//		}
-//	}
-//
-//	return isColored && !f.DisableColors
-//}
-//
-//// Format renders a single log entry
-//
-//
-//func (f *ModuleFormatter) printColored(b *bytes.Buffer, entry *Entry, keys []string, data Fields, timestampFormat string) {
-//	var levelColor int
-//	switch entry.Level {
-//	case DebugLevel, TraceLevel:
-//		levelColor = gray
-//	case WarnLevel:
-//		levelColor = yellow
-//	case ErrorLevel, FatalLevel, PanicLevel:
-//		levelColor = red
-//	default:
-//		levelColor = blue
-//	}
-//
-//	levelText := strings.ToUpper(entry.Level.String())
-//	if !f.DisableLevelTruncation {
-//		levelText = levelText[0:4]
-//	}
-//
-//	// Remove a single newline if it already exists in the message to keep
-//	// the behavior of logrus text_formatter the same as the stdlib log package
-//	entry.Message = strings.TrimSuffix(entry.Message, "\n")
-//
-//	caller := ""
-//
-//	if entry.HasCaller() {
-//		funcVal := fmt.Sprintf("%s()", entry.Caller.Function)
-//		fileVal := fmt.Sprintf("%s:%d", entry.Caller.File, entry.Caller.Line)
-//
-//		if f.CallerPrettyfier != nil {
-//			funcVal, fileVal = f.CallerPrettyfier(entry.Caller)
-//		}
-//		caller = fileVal + " " + funcVal
-//	}
-//
-//	if f.DisableTimestamp {
-//		fmt.Fprintf(b, "\x1b[%dm%s\x1b[0m%s %-44s ", levelColor, levelText, caller, entry.Message)
-//	} else if !f.FullTimestamp {
-//		fmt.Fprintf(b, "\x1b[%dm%s\x1b[0m[%04d]%s %-44s ", levelColor, levelText, int(entry.Time.Sub(baseTimestamp)/time.Second), caller, entry.Message)
-//	} else {
-//		fmt.Fprintf(b, "\x1b[%dm%s\x1b[0m[%s]%s %-44s ", levelColor, levelText, entry.Time.Format(timestampFormat), caller, entry.Message)
-//	}
-//	for _, k := range keys {
-//		v := data[k]
-//		fmt.Fprintf(b, " \x1b[%dm%s\x1b[0m=", levelColor, k)
-//		f.appendValue(b, v)
-//	}
-//}
-//
 func (f *ModuleFormatter) needsQuoting(text string) bool {
 	if f.QuoteEmptyFields && len(text) == 0 {
 		return true
@@ -365,8 +214,7 @@ func prefixFieldClashes(data Fields, fieldMap FieldMap, reportCaller bool) {
 }
 
 func (f *ModuleFormatter) isColored() bool {
-	isColored := f.ForceColors || (f.isTerminal && (runtime.GOOS != "windows"))
-
+	isColored := true
 	if f.EnvironmentOverrideColors {
 		if force, ok := os.LookupEnv("CLICOLOR_FORCE"); ok && force != "0" {
 			isColored = true
@@ -378,12 +226,6 @@ func (f *ModuleFormatter) isColored() bool {
 	}
 
 	return isColored && !f.DisableColors
-}
-
-func (f *ModuleFormatter) init(entry *Entry) {
-	if entry.Logger != nil {
-		f.isTerminal = checkIfTerminal(entry.Logger.Out)
-	}
 }
 
 
@@ -433,14 +275,5 @@ func (f *ModuleFormatter) printColored(b *bytes.Buffer, entry *Entry, keys []str
 		v := data[k]
 		fmt.Fprintf(b, " \x1b[%dm%s\x1b[0m=", levelColor, k)
 		f.appendValue(b, v)
-	}
-}
-
-func checkIfTerminal(w io.Writer) bool {
-	switch v := w.(type) {
-	case *os.File:
-		return terminal.IsTerminal(int(v.Fd()))
-	default:
-		return false
 	}
 }
