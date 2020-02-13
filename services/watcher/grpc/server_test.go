b00f573cb51122043623320d6628173481faab85
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2020-02-13 16:16:06 -0600

    Adding an error test.

--- services/watcher/grpc/server_test.go
+++ services/watcher/grpc/server_test.go
@@ -12,14 +12,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var testLogger = logger.New("server_test")
+
 func TestWatchTransaction(t *testing.T) {
 	eth.Mock()
 
 	ws := WatcherServer{
 		TxWatching: watching.New(),
-		log: logger.New("test"),
+		log: testLogger,
 	}
-	transaction, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf98210", QuoteId: "test"})
+	transaction, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf9821C", QuoteId: "test"})
 	assert.Equal(t, false, transaction.IsPending)
 	assert.Equal(t, nil, err)
+}
+
+func TestWatchTransactionFailure(t *testing.T) {
+	eth.Mock()
+
+	ws := WatcherServer{
+		TxWatching: watching.New(),
+		log: testLogger,
+	}
+	_, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf98219", QuoteId: "test"})
+	assert.Errorf(t, err, "testing")
 }
