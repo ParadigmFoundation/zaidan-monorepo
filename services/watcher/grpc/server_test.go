331e15144c72622c4118b8ba50d7f0ffdf5c0b2b
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2020-02-13 18:27:23 -0600

    Adding a test for the new code.

--- services/watcher/grpc/server_test.go
+++ services/watcher/grpc/server_test.go
@@ -2,6 +2,8 @@ package grpc
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"sort"
 	"testing"
 
 	"github.com/ParadigmFoundation/zaidan-monorepo/lib/go/logger"
@@ -35,4 +37,32 @@ func TestWatchTransactionFailure(t *testing.T) {
 	}
 	_, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf98219", QuoteId: "test"})
 	assert.Errorf(t, err, "testing")
+}
+
+func TestWatchTransactionAppendEndpoint(t *testing.T) {
+	eth.Mock()
+
+	testTxString := "0x71b044c65962a23ed50a6081177b2ec2711b32d9fb1c9b2c7a4b6d711bf98FF0"
+	testTxHash := common.HexToHash(testTxString)
+	ws := WatcherServer{
+		TxWatching: watching.New(),
+		log: testLogger,
+	}
+	_, watched := ws.TxWatching.IsWatched(testTxHash)
+	assert.False(t, watched)
+	r, err := ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: testTxString, QuoteId: "test", StatusUrls: []string{"one", "two"} })
+	assert.NoErrorf(t, err, "Unexpected error")
+	assert.True(t, r.IsWatched)
+	watcher, _ := ws.TxWatching.IsWatched(testTxHash)
+	assert.Equal(t, 2, len(watcher.UpdateUrls), "Not enough urls")
+
+	r, err = ws.WatchTransaction(context.Background(), &pb.WatchTransactionRequest{ TxHash: testTxString, QuoteId: "test", StatusUrls: []string{"five", "two"} })
+	assert.NoErrorf(t, err, "Unexpected error")
+	assert.True(t, r.IsWatched)
+	watcher, _ = ws.TxWatching.IsWatched(testTxHash)
+	assert.Equal(t, 3, len(watcher.UpdateUrls), "Not enough urls")
+	sort.Strings(watcher.UpdateUrls)
+	assert.Equal(t, watcher.UpdateUrls[sort.SearchStrings(watcher.UpdateUrls, "one")], "one")
+	assert.Equal(t, watcher.UpdateUrls[sort.SearchStrings(watcher.UpdateUrls, "two")], "two")
+	assert.Equal(t, watcher.UpdateUrls[sort.SearchStrings(watcher.UpdateUrls, "five")], "five")
 }
